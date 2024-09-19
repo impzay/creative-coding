2b79dbcc018c13a1851a3652764713062e8f07d7
impzay <ipadgett@mail.umw.edu>
2024-09-19 12:10:40 -0400

    Update sketch.js

--- unit2/2.2.grid/sketch.js
+++ unit2/2.2.grid/sketch.js
@@ -5,7 +5,23 @@ function setup() {
   // disable animation
   noLoop();
 }
+var skib = random(10);
 
 function draw() {
   background(220);
+
+  //for loop
+  translate(100,100);
+  for(let x = 0; x < 8; x++){
+    for(let y = 0; y < 8; y++){
+      push();
+      stroke('green');
+      strokeWeight(random(10));
+      fill(62, 195, 81);
+      translate(x * 100, y * 100);
+      rect(0,0,100,100);
+      pop();
+
+    }
+  }
 }
