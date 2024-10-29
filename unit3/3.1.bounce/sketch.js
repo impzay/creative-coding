d573b9b0eaf78f28ea7836e7b1d634452365a1ea
impzay <ipadgett@mail.umw.edu>
2024-10-29 11:45:28 -0400

    a

--- unit3/3.1.bounce/sketch.js
+++ unit3/3.1.bounce/sketch.js
@@ -1,36 +1,56 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 425);
 
-  squareX = random(width - 50);//starts the square anywhere inside canvas
-  squareY = random(height - 50);
+  squareX = random(400 - 50);//starts the square anywhere inside canvas
+  squareY = random(400 - 50);
 
   Xspeed = random(-10,10);//sets speed to random between -5 and 5
   Yspeed = random(-10,10);
 
+  squareX2 = random(800 - 50,425);//starts the square anywhere inside canvas
+  squareY2 = random(400 - 50);
+
+  Xspeed2 = random(-10,10);//sets speed to random between -5 and 5
+  Yspeed2 = random(-10,10);
 }
 
 //variables
 var squareX;
 var squareY;
+var squareX2;
+var squareY2;
 var Yspeed;
 var Xspeed;
+var Yspeed2;
+var Xspeed2;
 var squareColor = 'red';
 var score = 0;
+let img;
+
+function preload(){
+   img = loadImage('hinata.jpg');
+}
 
 function draw() { 
   background(0,0,0);
 
 
-  if(squareX < 0 || squareX > width - 50){
+  if(squareX < 0 || squareX > 400 - 50){
     Xspeed = -Xspeed;
   }
-  if(squareY < 0 || squareY > height - 50){
+  if(squareY < 0 || squareY > 400 - 50){
     Yspeed = -Yspeed;
   }
-  if(squareY <= 0 && squareX <= 0 || squareY >= height - 50 && squareX >= width - 50){
+  if(squareX2 < 425 || squareX2 > 800 - 50){
+    Xspeed2 = -Xspeed2;
+  }
+  if(squareY2 < 0 || squareY2 > 400 - 50){
+    Yspeed2 = -Yspeed2;
+  }
+  if(squareY <= 0 && squareX <= 0 || squareY >= 400 - 50 && squareX >= 400 - 50){
     squareColor = color(random(0,255),random(0,255),random(0,255));
     score = score + 50;
-  } else if(squareY <= 0 && squareX >= 350 || squareY >= height - 50 && squareX <= 0){
+  } else if(squareY <= 0 && squareX >= 350 || squareY >= 400 - 50 && squareX <= 0){
     squareColor = color(random(0,255),random(0,255),random(0,255));
     score = score + 50;
   }  
@@ -38,16 +58,20 @@ function draw() {
   if(mouseX > squareX && mouseX < squareX + 50 && mouseY > squareY && mouseY < squareY + 50){
     Xspeed = -Xspeed;
     Yspeed = -Yspeed;
-
+    squareColor = color(random(0,255),random(0,255),random(0,255));
+    score = score + 5;
   }
 
   squareX += Xspeed;
   squareY += Yspeed;
+  squareX2 += Xspeed2;
+  squareY2 += Yspeed2;
 
   //shape
   noStroke();
   fill(squareColor);
-  rect(squareX,squareY,50,50);
+  //rect(squareX,squareY,50,50);
+  rect(squareX2,squareY2,50,50);
 
 
   //score text
@@ -60,9 +84,15 @@ function draw() {
   fill('white');
   circle(mouseX,mouseY,15,15);
 
-  if(score == 100){
+  if(score >= 100){
     gameWon();
   }
+
+  fill('grey');
+  rect(400,0,25,800);
+  rect(0,400,825,25);
+
+  image(img,100,100,50,50);
 }
 
 function mouseClicked(){
--- unit3/3.2.follower/sketch.js
+++ unit3/3.2.follower/sketch.js
@@ -1,7 +1,47 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
 }
+  let debug = true;
+  var circleX = 50;
+  var circleY = 50;
+  var xSpeed;
+  var ySpeed;
+  var speedfactor = 1;
+  var img;
+
 
 function draw() {
   background(220);
+  
+  img = preload('hinata.jpg');
+  image(img,circleX,circleY,50,50);
+
+  fill('black');
+  circle(mouseX,mouseY,15,15);
+  if (mouseX > circleX){
+    xSpeed = speedfactor;
+  } else {
+    xSpeed = -speedfactor;
+  }
+
+  if (mouseY > circleY){
+    ySpeed = speedfactor;
+  } else {
+    ySpeed = -speedfactor;
+  }
+
+  circleX = circleX + xSpeed;
+  circleY = circleY + ySpeed;
+
+  if ((circleX + 25 == mouseX) && (circleY + 25 == mouseY)){
+    circleX = 50;
+    circleY = 50;
+  }
+
+  if(debug){
+    text("width: " + width, 30, 30);
+    text("height: " + height, 30, 45);
+    text("mouseX: " + mouseX, 30, 60);
+    text("mouseY: " + mouseY, 30, 75);
+  }
 }
