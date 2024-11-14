f9cb7b4627e4f8469a63ae16ca6bfcaf19a9b04c
impzay <ipadgett@mail.umw.edu>
2024-11-14 12:19:58 -0500

    s

--- unit4/4.2.houses-of-dust/sketch.js
+++ unit4/4.2.houses-of-dust/sketch.js
@@ -1,7 +1,32 @@
+var poem = 's';
+var fish = tracery.createGrammar(
+{
+  "material":[
+"SAND",
+"DUST",
+"BUSS",
+"FISH",
+"YOSHI",
+"SEAN"
+],
+  "origin": "A HOUSE OF #material#"
+}
+);
+
+poem = fish.flatten("#origin#");
+
 function setup() {
   createCanvas(800, 800);
+  noLoop();
 }
 
 function draw() {
   background(220);
+
+  textSize(17);
+  stroke(2);
+  text(poem,50,50)
+
+
+
 }
