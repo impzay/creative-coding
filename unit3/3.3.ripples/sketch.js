f8fe68983f4a86771b0173add69c192f82d0a968
impzay <ipadgett@mail.umw.edu>
2024-11-18 15:46:47 -0500

    s

--- unit3/3.3.ripples/sketch.js
+++ unit3/3.3.ripples/sketch.js
@@ -1,7 +1,47 @@
+/*
+ * Isaiah padgett 
+ */
+
+let ripplers = [];
+let op;
+
+
 function setup() {
   createCanvas(800, 800);
+  
 }
 
 function draw() {
-  background(220);
+  background(105, 169, 209);
+
+  for (let i = 0; i < ripplers.length; i++){ //iterates ripplers
+    ripplers[i].draw();
+  }
+
+  //stroke(255,255,255);
+  //text(op,50,50);
+
+}
+
+//mouse pressed spawns a new rippler at mouse location
+function mousePressed(){
+  ripplers.push(new Rippler(mouseX, mouseY));
+  //randomizes opacity 
+  op = random(0.2,5);
+}
+
+class Rippler{
+    constructor (x,y){ // constructor for the ripplers
+      this.x = x,
+      this.y = y,
+      this.diameter = 0
+      this.opacity = 255
+    }
+  draw(){ //draw function for ripplers 
+    this.diameter += 2;
+    this.opacity -= op;
+    noFill();
+    stroke(34, 76, 102,this.opacity);
+    ellipse(this.x, this.y, this.diameter);
+  }
 }
