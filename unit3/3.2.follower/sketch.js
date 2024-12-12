670dfd8faa2c2b21148cc58c2f974e265f2a64d5
impzay <ipadgett@mail.umw.edu>
2024-12-11 23:32:26 -0500

    comments for follower

--- unit3/3.2.follower/sketch.js
+++ unit3/3.2.follower/sketch.js
@@ -1,3 +1,5 @@
+  
+  //variables
   let debug = false;
   var circleX = 50;
   var circleY = 50;
@@ -7,28 +9,38 @@
   var img;
   var randomX, randomY;
   var score = 0;
+  
 function setup() {
   createCanvas(500, 500);
 }
 
 function preload(){
+  //loads image from github as follower
     img = loadImage('https://impzay.github.io/creative-coding/unit3/3.2.follower/hinata.jpg');
 }
 function draw() {
+  //selects two random values for new x and y
   randomX = random(50,450);
   randomY = random(50,450);
   background(220);
   
+  //follower
   image(img,circleX,circleY,50,50);
 
+
+  //mouse circle
   fill('black');
   circle(mouseX,mouseY,15,15);
+
+  //conditional statement for resetting the follower if it interacts with mouse
   if(circleX >= mouseX - 15 && circleX <= mouseX + 15 && circleY >= mouseY - 15 && circleY <= mouseY + 15){
     score -= 1;
     circleX = randomX;
     circleY = randomY;
   }
 
+
+  //sets the direction of the follower depending on the location of the mosue
   if (mouseX > circleX){
     xSpeed = speedfactor;
   } else {
@@ -41,10 +53,14 @@ function draw() {
     ySpeed =-speedfactor;
   }
 
+
+  //adds speed
   circleX += xSpeed;
   circleY += ySpeed;
 
+  //score text
   text("score!: "+score,30,30);
+  //debug
   if(debug){
     text("width: " + width, 30, 30);
     text("height: " + height, 30, 45);
